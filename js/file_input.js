262c8bce2769a82bf50e6ca5d05c2a68761e3d2d
InvYasiel <yasiel.dev@gmail.com>
2018-06-07 12:45:36 +0100

    8

--- js/file_input.js
+++ js/file_input.js
@@ -148,14 +148,7 @@ $(function() {
             area;
 
         if (result) {
-            if (result.boxes) {
-                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                result.boxes.filter(function (box) {
-                    return box !== result.box;
-                }).forEach(function (box) {
-                    Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-                });
-            }
+            
 
             if (result.box) {
                 Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
